5a76872154933518acd2ed83a3a9a1428d7530dc
Esha Rajbhandari <esha9277@gmail.com>
2024-04-29 07:31:16 +0545

    Setup backend service for calendar events

---
+++ calendar-events/backend/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+const dotenv = require("dotenv");
+dotenv.config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      port: 5432,
+      user: "postgres",
+      host: "localhost",
+      database: process.env.DATABASE,
+      password: process.env.PASSWORD,
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
